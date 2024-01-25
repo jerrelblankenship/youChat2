1075a4210c6d2490fd029a0ed101349b56d50456
Jerrel Blankenship <jerrelb@outlook.com>
2024-01-25 12:48:22 -0500

    Added environment variables and created the Firebase configuration.

---
+++ app/services/FirebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+import { initializeApp } from 'firebase/app'
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig)
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+})
+export const FIREBASE_DB = getFirestore(FIREBASE_APP)
